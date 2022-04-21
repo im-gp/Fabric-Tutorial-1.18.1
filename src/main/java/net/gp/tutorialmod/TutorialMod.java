f9c16364b356be8d1cc71c2f41dabb00a37272bb
theonlygp <gpvmgamer@gmail.com>
2022-04-20 20:28:39 -0500

    added comments

--- src/main/java/net/gp/tutorialmod/TutorialMod.java
+++ src/main/java/net/gp/tutorialmod/TutorialMod.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.Logger;
 
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
+	//test
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 	@Override
